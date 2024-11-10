b97007689d4bf8b169ef3c657dda24eb4191e0f4
Yurii Hetsko <getttzzz@gmail.com>
2024-11-10 21:58:43 +0100

    add grid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.yuriihetsko.fallingsand"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.yuriihetsko.fallingsand"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/yuriihetsko/fallingsand/MainActivity.kt
+++ app/src/main/java/com/yuriihetsko/fallingsand/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.yuriihetsko.fallingsand.ui.theme.FallingSandTheme
+
+const val PIXEL = 50f
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             FallingSandTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +37,64 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun MainScreen(modifier: Modifier = Modifier) {
+    var grid = remember { build2DArrayList(5, 5) }
+    val nextGrid = remember { build2DArrayList(5, 5) }
+
+    grid[3][3] = 1
+    grid[1][2] = 1
+
+    println("GETZ.<top>.FallingSandRoot--> grid=${grid}")
+
+    Canvas(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .background(Color.LightGray)
+    ) {
+
+
+        for (i in 0..5) {
+            for (j in 0..5) {
+                val calculatedColor = if (grid[i][j] == 0) Color.Black else Color.White
+                drawRect(color = calculatedColor, topLeft = Offset(i * PIXEL, j * PIXEL), size = Size(PIXEL, PIXEL))
+            }
+        }
+
+//        for (i in 0..5) {
+//            for (j in 0..5) {
+//                val state = grid[i][j]
+//                if (state == 1) {
+//                    val below = grid[i][j + 1]
+//                    if (below == 0) {
+//                        nextGrid[i][j] = 0
+//                        nextGrid[i][j + 1] = 1
+//                    }
+//                }
+//            }
+//        }
+//
+//        grid = nextGrid
+    }
+}
+
+fun build2DArrayList(rows: Int, columns: Int): ArrayList<ArrayList<Int>> {
+    val arrayList2D = ArrayList<ArrayList<Int>>(rows)
+
+    for (i in 0..rows) {
+        val row = ArrayList<Int>(columns)
+        for (j in 0..columns) {
+            row.add(0)
+        }
+        arrayList2D.add(row)
+    }
+
+    return arrayList2D
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     FallingSandTheme {
-        Greeting("Android")
+        MainScreen(Modifier.size(400.dp, 400.dp))
     }
 }
