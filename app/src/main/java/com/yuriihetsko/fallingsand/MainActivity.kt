61440c0a4171b03b27d54dff3464b14fff5b0886
Yurii Hetsko <getttzzz@gmail.com>
2024-11-11 11:14:17 +0100

    pixel starts fall down

--- app/src/main/java/com/yuriihetsko/fallingsand/MainActivity.kt
+++ app/src/main/java/com/yuriihetsko/fallingsand/MainActivity.kt
@@ -3,11 +3,16 @@ package com.yuriihetsko.fallingsand
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -16,6 +21,7 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.yuriihetsko.fallingsand.ui.theme.FallingSandTheme
 
@@ -24,11 +30,15 @@ const val PIXEL = 50f
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             FallingSandTheme {
                 val viewModel = viewModel<MainScreenViewModel>()
-                MainScreen(Modifier.fillMaxSize(), viewModel)
+                MainScreen(
+                    Modifier
+                        .fillMaxSize()
+                        .padding(WindowInsets.statusBars.asPaddingValues()), viewModel
+                )
             }
         }
     }
@@ -37,18 +47,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier, viewModel: MainScreenViewModel) {
     val grid = viewModel.grid.collectAsState()
-
-    Canvas(modifier = modifier.background(Color.LightGray)) {
-        println("GETZ.<top>.MainScreen--> draw grid")
-        for (i in 0..5) {
-            for (j in 0..5) {
-                val calculatedColor = if (grid.value.gridValue[i][j] == 0) Color.Black else Color.White
-                drawRect(color = calculatedColor, topLeft = Offset(i * PIXEL, j * PIXEL), size = Size(PIXEL, PIXEL))
+    println("GETZ.<top>.MainScreen--> grid.value.text=${grid.value.counter}")
+    Column(modifier = modifier) {
+        Text(text = "Some Text=${grid.value.counter}", fontSize = 40.sp)
+        Canvas(modifier = modifier.background(Color.LightGray)) {
+            for (i in 0..5) {
+                for (j in 0..5) {
+                    val calculatedColor = if (grid.value.gridValue[i][j] == 0) Color.Black else Color.White
+                    drawRect(color = calculatedColor, topLeft = Offset(i * PIXEL, j * PIXEL), size = Size(PIXEL, PIXEL))
+                }
             }
         }
     }
 }
-
 
 
 @Preview(showBackground = true)
--- app/src/main/java/com/yuriihetsko/fallingsand/MainScreenViewModel.kt
+++ app/src/main/java/com/yuriihetsko/fallingsand/MainScreenViewModel.kt
@@ -6,52 +6,54 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import kotlin.random.Random
 
 class MainScreenViewModel : ViewModel() {
 
-    data class GridState(val gridValue: ArrayList<ArrayList<Int>>)
+    data class GridState(val gridValue: ArrayList<ArrayList<Int>>, val counter: Int)
 
-    private val _grid = MutableStateFlow(GridState(build2DArrayList(5, 5)))
+    private val _grid = MutableStateFlow(GridState(build2DArrayList(5, 5), 0))
     val grid = _grid.asStateFlow()
 
     init {
-        _grid.value.gridValue[3][3] = 1
-        _grid.value.gridValue[1][2] = 1
-        _grid.value.gridValue[4][4] = 1
+        _grid.value.gridValue[1][1] = 1
 
         viewModelScope.launch {
             while (true) {
-                delay(1000)
+            delay(1000)
 
-//                val randomI = Random(System.currentTimeMillis()).nextInt(0, 6)
-//                val randomJ = Random(System.currentTimeMillis()).nextInt(0, 6)
-//                println("GETZ.MainScreenViewModel.--> randomI=$randomI randomJ=$randomJ")
+            var oldCounter = _grid.value.counter
 
-                val wtf = _grid.value.gridValue
-//                wtf[randomI][randomJ] = 1
+            val grid = _grid.value.gridValue
+            val nextGrid = build2DArrayList(5, 5)
 
-                wtf[0][0] = 1
-                wtf[1][1] = 1
 
-                _grid.value = GridState(wtf)
+            for (i in 0..4) {
+                for (j in 0..4) {
+                    val state = grid[i][j]
+                    println("GETZ.MainScreenViewModel.--> i=$i j=$j state=$state")
+
+                    if (state == 1) {
+
+                        val below = grid[i][j + 1]
+                        println("GETZ.MainScreenViewModel.--> i=$i j=$j below=$below")
+                        if (below == 0 && j < 5 - 1) {
+                            nextGrid[i][j] = 0
+                            nextGrid[i][j + 1] = 1
+                        } else {
+                            nextGrid[i][j] = 1
+                        }
+                    }
+                }
+
+
             }
+
+            val incremented = ++oldCounter
+            _grid.value = GridState(nextGrid, incremented)
         }
 
-        //        for (i in 0..5) {
-//            for (j in 0..5) {
-//                val state = grid[i][j]
-//                if (state == 1) {
-//                    val below = grid[i][j + 1]
-//                    if (below == 0) {
-//                        nextGrid[i][j] = 0
-//                        nextGrid[i][j + 1] = 1
-//                    }
-//                }
-//            }
-//        }
-//
-//        grid = nextGrid
+
+        }
     }
 
 
